bb37d20f7a42a2a46dfee32c772324182684a956
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-09 22:56:00 -0800

    Added comments throughout the js file to help illustrate how the page runs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// These variables get various elements from the page and allow me to easily reference them
 var currentDayEl = $("#currentDay");
 var nineAM = $("#9AM");
 var tenAM = $("#10AM");
@@ -10,12 +11,14 @@ var fourPM = $("#4PM");
 var fivePM = $("#5PM");
 var saveBtn = $(".saveBtn");
 
+// This function is in charge of setting the date at the top of the page
 function displayDate() {
   var rightNow = moment().format("dddd, MMMM Do");
 
   currentDayEl.text(rightNow);
 }
 
+// This function stores every input all at once in local storage so it can be accessed and retrieved later
 function setInfo() {
   localStorage.setItem("9AM", nineAM.val());
   localStorage.setItem("10AM", tenAM.val());
@@ -28,6 +31,7 @@ function setInfo() {
   localStorage.setItem("5PM", fivePM.val());
 }
 
+// This function gets all the info stored in local storage and sets the relevant text area to contain that info
 function getInfo() {
   var info9a = localStorage.getItem("9AM");
   var info10a = localStorage.getItem("10AM");
@@ -50,6 +54,7 @@ function getInfo() {
   fivePM.text(info5p);
 }
 
+// This function is in charge of checking a given hour block to see if it is in the past present or future and setting the appropiate class to it
 function timeChecker(hour) {
   var rightNow = moment().format("HH");
 
@@ -64,10 +69,12 @@ function timeChecker(hour) {
   }
 }
 
+// This interval runs the displayDate function every minute, allowing the date to stay accurate
 setInterval(() => {
   displayDate();
 }, 60000);
 
+// This interval runs the timeChecker function every minute on all the timeblocks to make sure they stay up to date and can actually be relied on
 setInterval(() => {
   timeChecker(nineAM);
   timeChecker(tenAM);
@@ -80,6 +87,7 @@ setInterval(() => {
   timeChecker(fivePM);
 }, 60000);
 
+// This bunch of calls are all the functions that need to run in order for the page to look how it should on initial load, after this call the heavy lifting shifts to the previous setIntervals to refresh the time on the page every minute to ensure it stays accurate
 getInfo();
 displayDate();
 timeChecker(nineAM);
@@ -92,6 +100,7 @@ timeChecker(threePM);
 timeChecker(fourPM);
 timeChecker(fivePM);
 
+// This onClick is what allows the user to save the information on the page. It works regardless of which save button is actually pressed so even if the user presses the incorrect button their information will still be saved
 saveBtn.on("click", function () {
   setInfo();
 });
